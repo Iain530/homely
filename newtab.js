adde37490b83c339f9a7b033942d0a2c5d6addb5
Iain Cattermole <iain_c@live.co.uk>
2018-07-18 01:16:10 +0100

    dynamic folder creation

--- newtab.js
+++ newtab.js
@@ -1,49 +1,10 @@
 window.addEventListener('DOMContentLoaded', function() {
-    const FAVICON_LINK = 'https://s2.googleusercontent.com/s2/favicons?domain_url=';
+    const HOMELY_FOLDER = 'Homely';
+    const SOCIAL_FOLDER = 'Social Media';
+    const VIDEO_FOLDER = 'Video';
+    const MUSIC_FOLDER = 'Music';
 
-    function add_attr(element, attr, value = null) {
-        let at = document.createAttribute(attr);
-        if (value) at.value = value;
-        element.setAttributeNode(at);
-    };
-
-    String.prototype.format = function() {
-        var args = arguments;
-        return this.replace(/{(\d+)}/g, function(match, number) {
-            return typeof args[number] != 'undefined' ?
-                args[number] :
-                match;
-        });
-    };
-
-    function build_bookmarks(bookmarks) {
-        let element = document.createDocumentFragment();
-        for (let bookmark of bookmarks) {
-            console.log(bookmark);
-            element.appendChild(build_link(bookmark));
-        }
-        return element;
-    }
-
-    function build_link(bookmark) {
-        let link = document.createElement('a');
-        link.href = bookmark.url;
-        link.className = "remove-style";
-
-        let box = document.createElement('div');
-        box.className = "box-link";
-
-        let favicon = document.createElement('img');
-        favicon.src = FAVICON_LINK + bookmark.url;
-
-        let name = document.createElement('div');
-        name.textContent = bookmark.title;
-
-        link.appendChild(box);
-        box.appendChild(favicon);
-        box.appendChild(name);
-        return link;
-    }
+    const bookmarks_api = browser.bookmarks;
 
     let is_focus = false;
 
@@ -58,30 +19,78 @@ window.addEventListener('DOMContentLoaded', function() {
         }
     });
 
+    function on_rejected(error) {
+        console.log(`An error: ${error}`);
+    }
+
+    function init_folders() {
+        bookmarks_api.create({
+            title: HOMELY_FOLDER,
+            type: 'folder'
+        }).then(function(node) {
+
+            let creating_folders = [
+                bookmarks_api.create({
+                    title: SOCIAL_FOLDER,
+                    type: 'folder',
+                    parentId: node.id
+                }),
+                bookmarks_api.create({
+                    title: VIDEO_FOLDER,
+                    type: 'folder',
+                    parentId: node.id
+                }),
+                bookmarks_api.create({
+                    title: MUSIC_FOLDER,
+                    type: 'folder',
+                    parentId: node.id
+                })
+            ];
+
+            Promise.all(creating_folders).then(populate_folders, on_rejected);
+        }, on_rejected);
+    }
+
+    function populate_folders(folders) {
+
+        display_folder(folders[0].parentId);
+    }
+
+    function display_folder(id) {
+        bookmarks_api.getSubTree(id).then(process_subtree, on_rejected);
+    }
+
+
     let unprocessed = [];
     let bookmarks = [];
     let folders = [];
-    browser.bookmarks.getTree(function(result) {
-        console.log(result[0].children[1].children);
-        for (let node of result) unprocessed.push(node);
-        while (unprocessed.length > 0) {
-            process_node(unprocessed.shift());
-        }
-        document.getElementById("here").appendChild(build_bookmarks(bookmarks));
-    });
+    bookmarks_api.search({
+        title: HOMELY_FOLDER
+    }).then(
+        bookmark_items => {
+            let folder = null;
+            for (let bookmark of bookmark_items) {
+                if (bookmark.type === 'folder') {
+                    folder = bookmark;
+                    break;
+                }
+            }
+            if (folder) {
+                display_folder(folder.id);
+            } else {
+                init_folders();
+            }
+        }, on_rejected);
 
-    function process_node(node) {
-        if (node.type === 'bookmark') {
-            bookmarks.push(node);
-        } else if (node.type === 'folder') {
-            folders.push(node);
-            process_folder(node);
-        }
-    }
+    function process_subtree(bookmark_items) {
+        if (bookmark_items.length == 0) return init_folders();
 
-    function process_folder(folder) {
-        for (let child of folder.children.reverse()) {
-            unprocessed.push(child);
+        for (let bookmark of bookmark_items[0].children) {
+            console.log(bookmark.children);
+            if (bookmark.type === 'folder') {
+                console.log(build_bookmarks(bookmark.children, bookmark.title));
+                document.getElementById("bookmarks").appendChild(build_bookmarks(bookmark.children, bookmark.title));
+            }
         }
     }
 
